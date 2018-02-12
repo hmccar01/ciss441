6376aaca0bfbd7e1cd3ee8bfd8f68c2256ebb0ae
hmccar01 <31674696+hmccar01@users.noreply.github.com>
2018-02-11 19:08:01 -0500

    Update a1.py

--- a1.py
+++ a1.py
@@ -2,16 +2,20 @@ import csv
 import json
 
 with open("GT.csv") as GT:
-	readCSV = csv.reader(GT, delimiter=',')
+	readCSV = list(csv.reader(GT, delimiter=','))
 	
-	GTData = list(readCSV)
+	keys = readCSV[0]
+	data = readCSV[1:]
+	
+	entries = []
+	
+	for row in data:
+		entries.append(dict(zip(keys, row)))
 
-for line in range(10):
-	print(GTData[line][:4])
-	
-print()
-for line in range(30,10,-1):
-	print(json.dumps(GTData[line][:4]))
-	
-with open("GT.json", "w") as GTjson:
-	json.dump(GTData, GTjson)
+	print('The last 100 terrorism targes:')
+	for attack in entries[-100:]:
+		print(attack['target1'])
+		
+		
+	with open("terror.json", "w") as file2:
+		json.dump(entries[-100:], file2)
